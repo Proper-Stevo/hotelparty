7fbeff53cba60d37245c037ce95c3b88fbe7b5e5
Stevo <steven.barrios92@gmail.com>
2024-01-19 19:55:58 -0800

    display component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+
+import HotelDisplay from "../components/displayHotel"
 export default function HomePage() {
     // Component code here
     return (
         <>
         ç
+        <HotelDisplay />
         </>
     )
   }
