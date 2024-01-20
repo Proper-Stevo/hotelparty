89f14653340dd27dd409aba20aa24cbdb08b04d8
Arely Rivera <arelyrivera1996@gmail.com>
2024-01-19 21:05:01 -0800

    redid file structure

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <header className="text-white">
+        <h1>Footer</h1>
+      </header>
+    );
+  }
+  
---
+++ src/components/homepage/index.js
@@ -0,0 +1,8 @@
+export default function Homepage() {
+    return (
+      <header className="text-white">
+        <h1>Homepage</h1>
+      </header>
+    );
+  }
+  
