2c2fd56fc8cb8a1674224ba06968ef96ce0de5bc
Stevo <steven.barrios92@gmail.com>
2024-01-20 21:37:33 -0800

    changes

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -3,22 +3,24 @@ export default function search() {
         <>
         <div>
             <div>
-                Image Goes here.
+                <img src="\search\hotel.jpg" className="h-96 w-96"/>
             </div>
-        
+
+            <div className="flex">
             <form>
                 <div class="mb-4">
-                    <label for="destinationDate" class="block text-gray-700 text-sm font-medium mb-2">Destination Date</label>
+                    <label for="destinationDate" class="block  text-sm font-medium mb-2">Destination Date</label>
                     <input type="date" id="destinationDate" name="destinationDate" class="border w-full p-2 rounded" />
                 </div>
 
                 <div class="mb-4">
-                    <label for="guestsAmount" class="block text-gray-700 text-sm font-medium mb-2">Guests Amount</label>
+                    <label for="guestsAmount" class="block  text-sm font-medium mb-2">Guests Amount</label>
                     <input type="number" id="guestsAmount" name="guestsAmount" class="border w-full p-2 rounded" />
                 </div>
 
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Search</button>
             </form>
+            </div>
             </div>
         </>
     )
