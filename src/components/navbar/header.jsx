b429b3b8b786a2234ae5be40ded63c7547c5ab6f
Stevo <steven.barrios92@gmail.com>
2024-02-08 19:56:45 -0800

    home back button

--- src/components/navbar/header.jsx
+++ src/components/navbar/header.jsx
@@ -5,7 +5,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Home', href: '#', current: true },
+  { name: 'Home', href: '/', current: true },
   { name: 'Search', href: '/search', current: false },
   { name: 'Sign Up', href: '/signup', current: false },
   { name: 'Sign In', href: '/signin', current: false },
