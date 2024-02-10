eddc1622e282d8c50a1013c0d8c84646bb3edd53
Arely Rivera <arelyrivera1996@gmail.com>
2024-02-09 22:22:07 -0800

    added placeholder for amenities

--- src/app/hotel/page.js
+++ src/app/hotel/page.js
@@ -1,6 +1,6 @@
 import HotelHero from "../../components/hotel/hotel_hero"
 import Hotel2ColumnLeft from "../../components/hotel/hotel_2_column_left"
-import HotelAmenities from "../../components/hotel/hotel_amenitites"
+import HotelAmenities from "../../components/hotel/hotel_amenities"
 import HotelBookNow from "../../components/hotel/hotel_book_now";
 import HotelContactUs from "../../components/hotel/hotel_contact_us";
 import Hotel2ColumnRight from "../../components/hotel/hotel_2_column_right";
--- src/components/hotel/hotel_2_column_right.js
+++ src/components/hotel/hotel_2_column_right.js
@@ -4,32 +4,38 @@ export default function Hotel2ColumnRight(props) {
   return (
     <>
       <div className="container p-8 my-16 mx-auto xl:px-0 flex flex-wrap mb-20 lg:gap-10 lg:flex-nowrap ">
-        <div
-          className="flex items-center justify-center w-full lg:w-1/2 lg:order-1"
-        >
+        <div className="flex items-center justify-center w-full lg:w-1/2 lg:order-1">
           <div>
             <Image
               src="/assets/homepage_hero_desktop.png"
-              width="521"
-              height="521"
               alt="Benefits"
-              className="object-cover"
+              className="w-full h-auto object-cover" // Use `object-cover` or `object-contain` as needed
+              layout="responsive" // Only if using Next.js Image and you want it to be responsive
+              width={521} // Original width
+              height={521} // Original height to maintain aspect ratio
             />
           </div>
         </div>
 
-        <div
-          className="flex flex-wrap items-center justify-center w-full lg:w-1/2"
-        >
+        <div className="flex flex-wrap items-center justify-center w-full lg:w-1/2">
           <div>
             <div className="flex flex-col w-full mt-4">
               <h3 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
-                title
+                Experience
               </h3>
 
               <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
-                desc
+                Nestled in the lush tropical paradise of Jimbaran, Bali, AYANA
+                Resort and Spa offers an enchanting escape for travelers seeking
+                luxury, relaxation, and breathtaking ocean views.
               </p>
+              <a
+                className="rounded w-1/3 border-2 border-neutral-50 text-center px-[12px] py-[14px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Get started
+              </a>
             </div>
           </div>
         </div>
