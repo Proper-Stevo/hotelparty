ffebc7860ded050649cd7830042bb007770dcb7c
Arely Rivera <arelyrivera1996@gmail.com>
2024-02-09 19:44:56 -0800

    added components to the hotel page

--- src/app/hotel/page.js
+++ src/app/hotel/page.js
@@ -1,8 +1,8 @@
 import HotelHero from "../../components/hotel/hotel_hero"
 import Hotel2Column from "../../components/hotel/hotel_2_column"
 import HotelAmenities from "../../components/hotel/hotel_amenitites"
-import HotelBookNow from "@/components/hotel/hotel_book_now";
-import HotelContactUs from "@/components/hotel/hotel_contact_us";
+import HotelBookNow from "../../components/hotel/hotel_book_now";
+import HotelContactUs from "../../components/hotel/hotel_contact_us";
 
 export default function Home() {
     return (
